ee9061285ef03018a6be94cba11a1975e676eea9
APTX4396 <77609544+APTX-4396@users.noreply.github.com>
2021-03-13 16:04:20 +0800

    Create eg4-4 选择法排序随机数.cpp

---
+++ Learn/第四章 构造数据类型及编译预处理/eg4-4 选择法排序随机数.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+void Selection(int a[], int n)
+{
+	int i, j, temp;
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j < n; j++)
+		{
+			if (a[i] > a[j])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
+
+void main()
+{
+	int i = 0;
+	int N = 100;
+	int a[10];
+	srand(time(0));
+	cout << "排序前：" << endl;
+	for (i = 0; i < 10; i++)
+	{
+		a[i] = rand() % N + 1;
+		cout << a[i] << "\t";
+	}
+	Selection(a, 10);
+	cout << "排序后：" << endl;
+	for (i = 0; i < 10; i++)
+		cout << a[i] << "\t";
+}
