14da67c276b11c0aa1bb9ea58a7b36ec7ea2b86e
APTX4396 <77609544+APTX-4396@users.noreply.github.com>
2021-03-27 23:00:08 +0800

    Create eg6-4.cpp

---
+++ Learn/第六章 面向对象程序设计/eg6-4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Sum {
+private:
+	int num;
+public:
+	Sum();
+	Sum(int a, int b);
+	void print();
+};
+Sum::Sum() {
+	num = 15;
+	cout << "调用默认的构造函数，num=" << num << endl;
+}
+Sum::Sum(int a, int b) {
+	num = a + b;
+	cout << "调用带参数构造函数计算a+b=" << num << endl;
+}
+void Sum::print() {
+	cout << "调用一般成员函数" << endl;
+}
+int main() {
+	Sum s;
+	Sum();
+	Sum(3, 5);
+	s.print();
+
+	return 0;
+}
